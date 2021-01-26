42bbdfde2e271fe5b43a162441f812e2f9660c98
AlexIonin <aleks_ionin@bk.ru>
2021-01-26 12:24:57 +0200

    add lab 9 - 1

--- lab9/task1.py
+++ lab9/task1.py
@@ -9,15 +9,13 @@
 
 from math import acos
 from math import degrees
+from math import sqrt
 
 class Triangle:
     def __init__(self, side_a, side_b, side_c):
         self.__side_a = side_a
         self.__side_b = side_b
         self.__side_c = side_c
-        self.__ang_ab = degrees(acos((self.__side_c ** 2 - self.__side_a ** 2 - self.__side_b ** 2)/(-2 * self.__side_a * self.__side_b)))
-        self.__ang_bc = degrees(acos((self.__side_a ** 2 - self.__side_b ** 2 - self.__side_c ** 2)/(-2 * self.__side_b * self.__side_c)))
-        self.__ang_ac = degrees(acos((self.__side_b ** 2 - self.__side_a ** 2 - self.__side_c ** 2)/(-2 * self.__side_a * self.__side_c)))
 
     @property
     def side_a(self):
@@ -52,49 +50,73 @@ class Triangle:
         else:
             raise ValueError
 
-    @property
-    def ang_ab(self):
-        return self.__ang_ab
+    # Вычислить периметр треугольника
+    def get_perimeter(self):
+        return(self.__side_a + self.__side_b + self.__side_c)
 
-    @ang_ab.setter
-    def ang_ab(self, w):
-        if w > 0:
-            self.__ang_ab = w
-        else:
-            raise ValueError
+    # Вычислить площадь треугольника
+    def get_area(self):
+        p = self.get_perimeter()/2
+        pa = p - self.__side_a
+        pb = p - self.__side_b
+        pc = p - self.__side_c
+        return sqrt(p * pa * pb * pc)
 
-    @property
-    def ang_bc(self):
-        return self.__ang_bc
+    # Вычислить высоты треугольника
+    def get_heights(self):
+        ha = (2 * self.get_area())/self.__side_a
+        hb = (2 * self.get_area())/self.__side_b
+        hc = (2 * self.get_area())/self.__side_c
+        return ha, hb, hc
 
-    @ang_bc.setter
-    def ang_bc(self, w):
-        if w > 0:
-            self.__ang_bc = w
-        else:
-            raise ValueError
+    # Определение вида треугольника
+    def triangle_type(self):
+        if self.__side_a == self.__side_b == self.__side_c:
+            print("Равносторонний треугольник")
+        elif self.__side_a == self.__side_b or self.__side_a == self.__side_c or self.__side_b == self.__side_c:
+            print("Равнобедренный треугольник")
+        if self.__ang_ac == 90 or self.__ang_ab == 90 or self.__ang_bc == 90:
+            print("Прямоугольный треугольник")
 
-    @property
-    def ang_ac(self):
-        return self.__ang_ac
-
-    @ang_ac.setter
-    def ang_ac(self, w):
-        if w > 0:
-            self.__ang_ac = w
-        else:
-            raise ValueError
-
-    def check_triangle(self):
-        angl_sum = self.__ang_ac + self.__ang_bc + self.__ang_ab
-        if angl_sum >= 179.9 and angl_sum <= 180.1:
+    # Пересчет углов треугольника по его сторонам
+    def calc(self):
+        try:
+            self.__ang_ab = degrees(acos((self.__side_c ** 2 - self.__side_a ** 2 - self.__side_b ** 2)/(-2 * self.__side_a * self.__side_b)))
+            self.__ang_bc = degrees(acos((self.__side_a ** 2 - self.__side_b ** 2 - self.__side_c ** 2)/(-2 * self.__side_b * self.__side_c)))
+            self.__ang_ac = degrees(acos((self.__side_b ** 2 - self.__side_a ** 2 - self.__side_c ** 2)/(-2 * self.__side_a * self.__side_c)))
             return True
-        else:
+        except Exception as e:
             return False
 
-a = Triangle(2, 2, 2)
-print(a.side_a, a.side_b, a.side_c, a.ang_ac, a.ang_ab, a.ang_bc)
-print(a.check_triangle())
+    # Вывод информации о треугольнике
+    def output(self):
+        print(f"Стороны треугольника: {a.side_a}, {a.side_b}, {a.side_c}")
+        if self.calc():
+            print(f"Углы треугольника: {a.__ang_ac}, {a.__ang_ab}, {a.__ang_bc}")
+            print(f"Площадь треугольника {self.get_area()}")
+            print(f"Периметр треугольника {self.get_perimeter()}")
+            print(f"Высоты сторон треугольника {self.get_heights()}")
+            self.triangle_type()
+        else:
+            print("Невозможно построить треугольник с такими сторонами")
 
+# Создаем экземпляр класса
+a = Triangle(1, 1, 100)
+# Выводим информацию о треугольнике
+a.output()
 
+# Переопределяем стороны треугольника
+a.side_a = 2
+a.side_b = 2
+a.side_c = 2
 
+# Выводим информацию о треугольнике
+a.output()
+
+# Переопределяем стороны треугольника
+a.side_a = 3
+a.side_b = 4
+a.side_c = 5
+
+# Выводим информацию о треугольнике
+a.output()
